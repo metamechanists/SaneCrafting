cb477247a389024e36da8cb12252a3d87e2da97b
LordIdra <percy.baker.0@gmail.com>
2024-07-21 21:22:54 +0100

    bruh

--- src/main/java/org/metamechanists/sanecrafting/SaneCrafting.java
+++ src/main/java/org/metamechanists/sanecrafting/SaneCrafting.java
@@ -5,29 +5,12 @@
 package org.metamechanists.sanecrafting;
 
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
-import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.core.multiblocks.MultiBlock;
-import io.github.thebusybiscuit.slimefun4.core.multiblocks.MultiBlockMachine;
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.EnhancedCraftingTable;
 import lombok.Getter;
 import lombok.NonNull;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
+import org.metamechanists.sanecrafting.patches.EnhancedCraftingTablePatch;
 
 
 public final class SaneCrafting extends JavaPlugin implements SlimefunAddon {
@@ -38,95 +21,8 @@ public final class SaneCrafting extends JavaPlugin implements SlimefunAddon {
     public void onEnable() {
         instance = this;
 
-        // lol
-        Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
-            EnhancedCraftingTable enhancedCraftingTable = null;
-            for (SlimefunItem item : Slimefun.getRegistry().getEnabledSlimefunItems()) {
-                if (item instanceof EnhancedCraftingTable table) {
-                    enhancedCraftingTable = table;
-                }
-            }
-            if (enhancedCraftingTable == null) {
-                getServer().getLogger().severe("Failed to initialise SaneCrafting; EnhancedCraftingTable does not exist!");
-                return;
-            }
-
-            List<ItemStack[]> recipes = enhancedCraftingTable.getRecipes();
-            for (int j = 0; j < recipes.size(); j += 2) {
-                ItemStack[] input = recipes.get(j);
-                ItemStack output = recipes.get(j+1)[0];
-
-                List<ItemStack> items = Arrays.asList(input);
-
-                // Convert to shape
-                String itemCharacters = "abcdefghi";
-                List<String> shape = new ArrayList<>(List.of("abc", "def", "ghi"));
-                Map<Character, ItemStack> ingredients = new HashMap<>();
-                for (int y = 0; y < 3; y++) {
-                    for (int x = 0; x < 3; x++) {
-                        int i = x*3 + y;
-                        char character = itemCharacters.charAt(i);
-                        ItemStack itemStack = items.get(i);
-                        if (itemStack == null) {
-                            shape.set(y, shape.get(y).replace(character, ' '));
-                        } else {
-                            ingredients.put(character, itemStack);
-                        }
-                    }
-                }
-
-                // Trim vertical
-                for (int y = shape.size() - 1; y > 0; y--) {
-                    if (Objects.equals(shape.get(y), "   ")) {
-                        shape.remove(y);
-                    }
-                }
-
-                // Skip if no recipe (just in case)
-                if (shape.isEmpty()) {
-                    continue;
-                }
-
-                // Trim horizontal
-                for (int x = shape.get(0).length() - 1; x > 0; x--) {
-                    boolean allRowsEmptyAtX = true;
-                    for (int y = shape.size() - 1; y > 0; y--) {
-                        if (shape.get(y).charAt(x) != ' ') {
-                            allRowsEmptyAtX = false;
-                            break;
-                        }
-                    }
-                    if (allRowsEmptyAtX) {
-                        for (int y = shape.size() - 1; y > 0; y--) {
-                            getServer().getLogger().info("yeeting " + shape.get(y));
-                            String newRow = new StringBuilder(shape.get(y))
-                                    .deleteCharAt(x)
-                                    .toString();
-                            shape.set(y, newRow);
-                        }
-                    }
-                }
-
-                getServer().getLogger().info(Arrays.toString(shape.toArray()));
-
-                // Use index to avoid ID clash if two recipes for same item
-                String normalisedName = PlainTextComponentSerializer.plainText()
-                        .serialize(output.displayName())
-                        .toLowerCase()
-                        .replace(' ', '_')
-                        .replaceAll("[^a-z0-9/._\\-]", ""); // remove characters not allowed in id
-                String id = "sanecrafting_" + j / 2 + "_" + normalisedName;
-
-                getServer().getLogger().info(id);
-
-                ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(this, id), output);
-                recipe.shape(shape.toArray(new String[]{}));
-                for (Entry<Character, ItemStack> entry : ingredients.entrySet()) {
-                    recipe.setIngredient(entry.getKey(), entry.getValue());
-                }
-                getServer().addRecipe(recipe);
-            }
-        }, 1);
+        // Patches applied on first tick to ensure everything has loaded
+        Bukkit.getScheduler().scheduleSyncDelayedTask(this, EnhancedCraftingTablePatch::apply, 1);
     }
 
     @Override
---
+++ src/main/java/org/metamechanists/sanecrafting/patches/EnhancedCraftingTablePatch.java
@@ -0,0 +1,135 @@
+package org.metamechanists.sanecrafting.patches;
+
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.EnhancedCraftingTable;
+import lombok.experimental.UtilityClass;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.jetbrains.annotations.Nullable;
+import org.metamechanists.sanecrafting.SaneCrafting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+
+
+@UtilityClass
+public class EnhancedCraftingTablePatch {
+    private EnhancedCraftingTablePatch() {}
+
+    private @Nullable List<ItemStack[]> getRecipes() {
+        EnhancedCraftingTable enhancedCraftingTable = null;
+        for (SlimefunItem item : Slimefun.getRegistry().getEnabledSlimefunItems()) {
+            if (item instanceof EnhancedCraftingTable table) {
+                enhancedCraftingTable = table;
+            }
+        }
+
+        if (enhancedCraftingTable == null) {
+            Bukkit.getLogger().severe("Failed to initialise SaneCrafting; EnhancedCraftingTable does not exist!");
+            return null;
+        }
+
+        return enhancedCraftingTable.getRecipes();
+    }
+
+    private void convertRecipe(List<ItemStack> input, ItemStack output, int recipeIndex) {
+        // Convert to shape
+        String itemCharacters = "abcdefghi";
+        List<String> shape = new ArrayList<>(List.of("abc", "def", "ghi"));
+        Map<Character, ItemStack> ingredients = new HashMap<>();
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                int i = x * 3 + y;
+                char character = itemCharacters.charAt(i);
+                ItemStack itemStack = input.get(i);
+                if (itemStack == null) {
+                    shape.set(y, shape.get(y).replace(character, ' '));
+                } else {
+                    ingredients.put(character, itemStack);
+                }
+            }
+        }
+
+        // Trim vertical
+        for (int y = shape.size() - 1; y > 0; y--) {
+            if (Objects.equals(shape.get(y), "   ")) {
+                shape.remove(y);
+            }
+        }
+
+        // Skip if no recipe (just in case)
+        if (shape.isEmpty()) {
+            return;
+        }
+
+        // Trim horizontal
+        for (int x = shape.get(0).length() - 1; x > 0; x--) {
+            boolean allRowsEmptyAtX = true;
+            for (int y = shape.size() - 1; y > 0; y--) {
+                if (shape.get(y).charAt(x) != ' ') {
+                    allRowsEmptyAtX = false;
+                    break;
+                }
+            }
+            if (allRowsEmptyAtX) {
+                for (int y = shape.size() - 1; y > 0; y--) {
+                    String newRow = new StringBuilder(shape.get(y))
+                            .deleteCharAt(x)
+                            .toString();
+                    shape.set(y, newRow);
+                }
+            }
+        }
+
+        // Use index to avoid ID clash if two recipes for same item
+        String normalisedName = PlainTextComponentSerializer.plainText()
+                .serialize(output.displayName())
+                .toLowerCase()
+                .replace(' ', '_')
+                .replaceAll("[^a-z0-9/._\\-]", ""); // remove characters not allowed in id
+        String id = "sanecrafting_" + recipeIndex + "_" + normalisedName;
+
+        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(SaneCrafting.getInstance(), id), output);
+        recipe.shape(shape.toArray(new String[]{}));
+        for (Entry<Character, ItemStack> entry : ingredients.entrySet()) {
+            recipe.setIngredient(entry.getKey(), entry.getValue());
+        }
+        Bukkit.getServer().addRecipe(recipe);
+
+        throw new RuntimeException("oh no");
+    }
+
+    public void apply() {
+        List<ItemStack[]> recipes = getRecipes();
+        if (recipes == null) {
+            return;
+        }
+
+        int changedRecipes = 0;
+        for (int j = 0; j < recipes.size(); j += 2) {
+            ItemStack[] input = recipes.get(j);
+            ItemStack output = recipes.get(j + 1)[0];
+
+            try {
+                convertRecipe(Arrays.asList(input), output, j / 2);
+            } catch (RuntimeException e) {
+                String name = PlainTextComponentSerializer.plainText().serialize(output.displayName());
+                Bukkit.getLogger().severe("Failed to convert recipe for " + name + ": " + e);
+                continue;
+            }
+
+            changedRecipes++;
+        }
+
+        Bukkit.getLogger().info("Converted " + changedRecipes + " Enhanced Crafting Table recipes to regular Crafing Table recipes");
+    }
+}
